ce9cb475989fa56c19be53d77bc30116c3930d0c
kbws13 <hsy040506@163.com>
2025-08-10 21:14:36 +0800

    feat(kcache): 添加 singleflight 优化
    - 引入 singleflight 包，实现对并发请求的优化
    - 在 Group 结构中添加 loader 字段，用于协调并发请求
    - 重构 load函数，使用 singleflight.Group.Do 方法确保每个 key 只被获取一次
    - 新增 singleflight 包实现，提供并发请求的协调功能

--- kcache/kcache.go
+++ kcache/kcache.go
@@ -2,6 +2,7 @@ package kcache
 
 import (
 	"fmt"
+	"kcache/singleflight"
 	"log"
 	"sync"
 )
@@ -25,6 +26,9 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -43,6 +47,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -72,15 +77,23 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	view, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[KCache] Failed to get from peer", err)
 			}
-			log.Println("[KCache] Failed to get from peer", err)
 		}
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return view.(ByteView), nil
 	}
-	return g.getLocally(key)
+	return
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
